d82f0bf54c64133483393be39c14da7b13d0d1be
Fernando Di Masi <wildiney@gmail.com>
2021-09-06 15:46:59 -0300

    join csv data for notebook

--- Email Reports/join-csv.py
+++ Email Reports/join-csv.py
@@ -7,28 +7,22 @@ extension = 'csv'
 
 
 def getDataFolders():
-    all_folders = [name for name in os.listdir(
-        os.getcwd()) if os.path.isdir(os.path.join(os.getcwd(), name))]
-    print(all_folders)
-    unused_folders = ['.ipynb_checkpoints', '.vscode',
-                      'join-csv.py', '.git', "00000000 - Campaña"]
+    all_folders = [name for name in os.listdir(os.getcwd()) if os.path.isdir(os.path.join(os.getcwd(), name))]
+    unused_folders = ['.ipynb_checkpoints', '.vscode', 'join-csv.py', '.git', "00000000 - Campaña"]
     folders = [item for item in all_folders if item not in unused_folders]
     return folders
 
 
 dirs = getDataFolders()
-# print(os.getcwd())
 
-# print(dirs)
+
 for dir in dirs:
     all_files = [i for i in glob.glob(os.getcwd()+"\\"+dir+"\\"+"*.csv")]
     print(all_files)
     print("--")
 
-    combined_csv = pd.concat(
-        [pd.read_csv(f, encoding='UTF-16 LE') for f in all_files])
-    combined_csv.to_csv(os.path.join(os.getcwd(), "..", "Email Send Analisys",
-                        "data", dir+".csv"), index=False, encoding="utf-8")
+    combined_csv = pd.concat([pd.read_csv(f, encoding='UTF-16 LE') for f in all_files])
+    combined_csv.to_csv(os.path.join(os.getcwd(), "..", "Email Send Analisys", "data", dir+".csv"), index=False, encoding="utf-8")
 
 # for file in all_filenames:
 #     if file != filename:
